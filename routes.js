29fe4447cbedf239b5f3fa7e0482f0a7c7b26cca
yse-yoo <youhei_yoshikawa@yse-c.net>
2021-12-03 13:44:17 +0900

    プロフィール修正

---
+++ routes.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const router = express.Router()
+
+const default_login_name = process.env.LOGIN_NAME
+const default_passsword = process.env.PASSWORD
+
+router.get('/login', (req, res) => {
+    res.render('login/index.ejs')
+})
+
+router.post('/auth', (req, res) => {
+    let message = 'ログインできません'
+    const login_name = req.body.login_name
+    const password = req.body.password
+
+    if (login_name == default_login_name
+        && password == default_passsword) {
+        message = 'ログインしました'
+    }
+    res.send(message)
+})
+
+router.get('/', (req, res) => {
+    let data = {}
+    data.title = 'トップページ'
+    res.render('index.ejs', data)
+})
+
+router.get('/profile', (req, res) => {
+    let user = {
+        id: 1,
+        name: '横浜　太郎',
+        birthplace: '横浜',
+        hobby: ['旅行', 'グルメ', 'スポーツ'],
+    }
+    let data = {}
+    data.title = 'プロフィール'
+    data.user = user
+
+    res.render('profile/index.ejs', data)
+})
+
+router.get('/item/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    res.render('item/show.ejs')
+})
+
+module.exports = router
